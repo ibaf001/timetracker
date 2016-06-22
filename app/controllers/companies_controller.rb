cd939cf45fe07b4a431757c2dd705068d543c97d
ibo <ibo.b@tcs.com>
2016-06-22 16:34:54 -0400

    Added edit forms

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -27,5 +27,19 @@ class CompaniesController < ApplicationController
     end
   end
 
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    if @company.update(params[:company].permit(:name))
+      flash[:notice] = 'Company Updated'
+      redirect_to @company
+    else
+      render 'edit'
+    end
+  end
+
 
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -25,5 +25,19 @@ class WorksController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @work = Work.find(params[:id])
+  end
+
+  def update
+    @work = Work.find(params[:id])
+    if @work.update(params[:work].permit(:project_id, :user_id, :datetimeperformed, :hours))
+      flash[:notice] = 'Work Updated'
+      redirect_to @work
+    else
+      render 'edit'
+    end
+  end
  
 end
