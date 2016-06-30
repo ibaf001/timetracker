8cda326253d0e78a8c1f2e1bdb1cbebddd3100a1
ibo <ibo.b@tcs.com>
2016-06-30 14:14:06 -0400

    Added test for work model

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -4,4 +4,10 @@ class WorkTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "work should not save with no associated user" do
+    work = Work.first
+    assert work.valid?, "work should be valid"
+    work.user_id = nil
+    assert_not work.valid?, "work should be invalid" 
+  end
 end
